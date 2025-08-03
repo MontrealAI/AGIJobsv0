88aceda9ef4d8055233525b44780d693f81fb577
MONTREAL.AI <info@montreal.ai>
2025-08-03 18:07:59 -0400

    docs: update readme and tooling

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,28 @@
+
 import { ethers } from "hardhat";
 
 async function main() {
-  const agiJobManagerV1 = await ethers.deployContract("AGIJobManagerV1");
+  const {
+    AGI_TOKEN_ADDRESS,
+    BASE_IPFS_URL,
+    ENS_ADDRESS,
+    NAME_WRAPPER_ADDRESS,
+    CLUB_ROOT_NODE,
+    AGENT_ROOT_NODE,
+    VALIDATOR_MERKLE_ROOT,
+    AGENT_MERKLE_ROOT,
+  } = process.env;
+
+  const agiJobManagerV1 = await ethers.deployContract("AGIJobManagerV1", [
+    AGI_TOKEN_ADDRESS,
+    BASE_IPFS_URL,
+    ENS_ADDRESS,
+    NAME_WRAPPER_ADDRESS,
+    CLUB_ROOT_NODE,
+    AGENT_ROOT_NODE,
+    VALIDATOR_MERKLE_ROOT,
+    AGENT_MERKLE_ROOT,
+  ]);
   await agiJobManagerV1.waitForDeployment();
 
   console.log(`AGIJobManagerV1 deployed to: ${agiJobManagerV1.target}`);
