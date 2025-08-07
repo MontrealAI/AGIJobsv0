2bbb2edf96454a1d9191d56bd338d6acea3602b1
MONTREAL.AI <info@montreal.ai>
2025-08-07 13:01:05 -0400

    feat: add modular job registry v2

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,12 @@ require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: { version: '0.8.21', settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true } },
+  solidity: {
+    compilers: [
+      { version: '0.8.23', settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true } },
+      { version: '0.8.21', settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true } }
+    ]
+  },
   paths: {
     sources: './contracts'
   },
