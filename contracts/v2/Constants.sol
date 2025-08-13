dc2511b87ae294922408b7a0a3466b8a1e8f7343
MONTREAL.AI <info@montreal.ai>
2025-08-13 17:32:17 -0400

    refactor: use shared AGIALPHA constant

---
+++ contracts/v2/Constants.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+// Shared AGI Jobs v2 constants.
+address constant AGIALPHA = 0x2e8Fb54C3eC41F55F06C1F082C081a609EaA4ebe;
--- contracts/v2/GovernanceReward.sol
+++ contracts/v2/GovernanceReward.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.25;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {AGIALPHA} from "./Constants.sol";
 import {IFeePool} from "./interfaces/IFeePool.sol";
 import {IStakeManager} from "./interfaces/IStakeManager.sol";
 
@@ -17,8 +18,7 @@ contract GovernanceReward is Ownable {
     uint256 public constant ACCUMULATOR_SCALE = 1e12;
 
     /// @notice default $AGIALPHA token used when no token is specified
-    address public constant DEFAULT_TOKEN =
-        0x2e8Fb54C3eC41F55F06C1F082C081a609EaA4ebe;
+    address public constant DEFAULT_TOKEN = AGIALPHA;
 
     /// @notice default epoch length when constructor param is zero
     uint256 public constant DEFAULT_EPOCH_LENGTH = 1 weeks;
