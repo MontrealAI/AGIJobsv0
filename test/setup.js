6cac40e52b2897da0a08e9a1d1c98a5068691ff4
MONTREAL.AI <info@montreal.ai>
2025-08-30 09:17:23 -0400

    refactor: move mock erc20 to test utils

--- test/setup.js
+++ test/setup.js
@@ -4,8 +4,9 @@ const { AGIALPHA } = require("../scripts/constants");
 let snapshotId;
 
 before(async function () {
+  // Load the test utility ERC20 used to stub the AGIALPHA token
   const artifact = await artifacts.readArtifact(
-    "contracts/legacy/MockERC20.sol:MockERC20"
+    "contracts/test/MockERC20.sol:MockERC20"
   );
   await network.provider.send("hardhat_setCode", [
     AGIALPHA,
