133da2e4151290ce0a466c16e5f862f5347551c6
MONTREAL.AI <info@montreal.ai>
2025-08-30 20:11:45 -0400

    chore: update MockERC20 pragma

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity ^0.8.25;
 
 // @dev Test utility token used to provide an ERC20 code stub for AGIALPHA
 // in Hardhat tests. This mock contract deploys a basic ERC20 and allows
