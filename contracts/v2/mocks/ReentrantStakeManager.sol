5afb42da32e21eb25368d9ff21f3be2a99a12420
MONTREAL.AI <info@montreal.ai>
2025-09-10 16:29:18 -0400

    feat: update to solidity 0.8.30

--- contracts/v2/mocks/ReentrantStakeManager.sol
+++ contracts/v2/mocks/ReentrantStakeManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.30;
 
 import {IStakeManager} from "../interfaces/IStakeManager.sol";
 import {IValidationModule} from "../interfaces/IValidationModule.sol";
