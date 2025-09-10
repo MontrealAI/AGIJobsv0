1840e05ff45e343d9d665bae0faabc82f7b9ebad
MONTREAL.AI <info@montreal.ai>
2025-09-10 14:22:17 -0400

    chore: update v2 contracts to solidity 0.8.29

--- contracts/v2/modules/KlerosDisputeModule.sol
+++ contracts/v2/modules/KlerosDisputeModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.29;
 
 import {IJobRegistry} from "../interfaces/IJobRegistry.sol";
 import {IDisputeModule} from "../interfaces/IDisputeModule.sol";
