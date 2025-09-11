da8202e208face5d70f132c74d24e027f3902b65
MONTREAL.AI <info@montreal.ai>
2025-09-10 20:56:37 -0400

    chore: upgrade to solidity 0.8.30

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-
-const configPath = path.join(__dirname, '..', 'config', 'agialpha.json');
-const { address, decimals } = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-
-module.exports = {
-  AGIALPHA: address,
-  AGIALPHA_DECIMALS: decimals,
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AGIALPHA_DECIMALS = exports.AGIALPHA = void 0;
+var fs = require("fs");
+var path = require("path");
+var configPath = path.join(__dirname, '..', 'config', 'agialpha.json');
+var _a = JSON.parse(fs.readFileSync(configPath, 'utf8')), address = _a.address, decimals = _a.decimals;
+// Canonical $AGIALPHA token address on Ethereum mainnet.
+exports.AGIALPHA = address;
+// Standard decimals for $AGIALPHA.
+exports.AGIALPHA_DECIMALS = decimals;
