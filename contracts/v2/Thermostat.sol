31280024fa00766933db06a2573969572c0c6909
MONTREAL.AI <info@montreal.ai>
2025-09-12 11:31:53 -0400

    feat: expand thermostat controls

--- contracts/v2/Thermostat.sol
+++ contracts/v2/Thermostat.sol
@@ -23,6 +23,7 @@ contract Thermostat is Ownable {
     event TemperatureUpdated(int256 newTemp);
     event RoleTemperatureUpdated(Role role, int256 temp);
     event PIDUpdated(int256 kp, int256 ki, int256 kd);
+    event TemperatureBoundsUpdated(int256 minTemp, int256 maxTemp);
     event Tick(int256 emission, int256 backlog, int256 sla, int256 newTemp);
 
     constructor(int256 _temp, int256 _min, int256 _max) Ownable(msg.sender) {
@@ -39,10 +40,34 @@ contract Thermostat is Ownable {
         emit PIDUpdated(_kp, _ki, _kd);
     }
 
+    /// @notice Sets a new system temperature within bounds.
+    function setSystemTemperature(int256 temp) external onlyOwner {
+        require(temp > 0 && temp >= minTemp && temp <= maxTemp, "temp");
+        systemTemperature = temp;
+        emit TemperatureUpdated(temp);
+    }
+
+    /// @notice Updates minimum and maximum allowable temperatures.
+    function setTemperatureBounds(int256 _min, int256 _max) external onlyOwner {
+        require(_min > 0 && _max > _min, "bounds");
+        minTemp = _min;
+        maxTemp = _max;
+        if (systemTemperature < minTemp) systemTemperature = minTemp;
+        if (systemTemperature > maxTemp) systemTemperature = maxTemp;
+        emit TemperatureBoundsUpdated(_min, _max);
+        emit TemperatureUpdated(systemTemperature);
+    }
+
     function setRoleTemperature(Role r, int256 temp) external onlyOwner {
         require(temp > 0 && temp >= minTemp && temp <= maxTemp, "bounds");
         roleTemps[r] = temp;
         emit RoleTemperatureUpdated(r, temp);
+    }
+
+    /// @notice Removes a role-specific temperature override.
+    function unsetRoleTemperature(Role r) external onlyOwner {
+        delete roleTemps[r];
+        emit RoleTemperatureUpdated(r, 0);
     }
 
     function getRoleTemperature(Role r) public view returns (int256) {
