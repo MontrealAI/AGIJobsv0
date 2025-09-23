91e47c5209dcfb852580ee04c3cdf0a7320664bd
MONTREAL.AI <info@montreal.ai>
2025-09-22 23:44:43 -0400

    chore: harden ci workflows

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
   istanbulReporter: ['json-summary', 'lcov', 'text'],
-  skipFiles: ['legacy', 'mocks', 'test'],
-  testCommand: 'COVERAGE_ONLY=1 npx hardhat test',
+  skipFiles: ['contracts/test', 'contracts/mocks'],
+  mocha: { grep: '@coverage-skip', invert: true },
 };
