0fe391c99edc02c2b0190d4648a8f3e3b4948971
MONTREAL.AI <info@montreal.ai>
2025-12-15 09:45:36 -0500

    Add dry-run support for governance alpha v17 demo

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
     "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json", diagnostics: false }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/services/culture-graph-indexer/package.json",
+    "<rootDir>/demo/CULTURE-v0/indexers/culture-graph-indexer/package.json",
+  ],
 };
